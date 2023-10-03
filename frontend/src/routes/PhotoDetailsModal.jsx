82958909441636562493cee7d2e470d96d52c0a9
callmeteni <isaacogunlaja9@gmail.com>
2023-10-02 20:59:50 -0500

    removed comments from files

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,7 +6,7 @@ import PhotoList from 'components/PhotoList';
 import PhotoFavButton from 'components/PhotoFavButton';
 
 
-const PhotoDetailsModal = ({isOpen, onClose,photoData,onFavouriteToggle,isSelected }) => {
+const PhotoDetailsModal = ({ isOpen, onClose, photoData, onFavouriteToggle, isSelected }) => {
   return (
     <div className={`photo-details-modal ${isOpen ? 'open' : 'close'}`}>
       <button className="photo-details-modal__close-button" onClick={onClose}>
@@ -24,33 +24,25 @@ const PhotoDetailsModal = ({isOpen, onClose,photoData,onFavouriteToggle,isSelect
             className="photo-details-modal__image"
           />
           <div>
-            <div className = 'photo-details-modal__photographer-details'>
-              
-            
+            <div className='photo-details-modal__photographer-details'>
+
+
               <div className="photo-list__user-details">
-                <img className = 'photo-list__user-profile' src={photoData.user.profile} alt={`${photoData.user.username}'s profile`} />
+                <img className='photo-list__user-profile' src={photoData.user.profile} alt={`${photoData.user.username}'s profile`} />
                 <div className="photo-list__user-info" >
                   {photoData.user.name}
-                  <p className = 'photo-list__user-location'>{photoData.location.city}, {photoData.location.country}</p>
+                  <p className='photo-list__user-location'>{photoData.location.city}, {photoData.location.country}</p>
                 </div>
               </div>
-              
+
             </div>
             <p>Similar Photos</p>
-            <PhotoList photoDatas ={photoData?.similar_photos} selectedFavourites = {[]} />
-            {/* {Object.values(photoData.similar_photos).map((similarPhoto) => (
-              <img
-                key={similarPhoto.id}
-                src={similarPhoto.urls.regular} // Display similar photos (you can adjust the URL accordingly)
-                alt={`Photo by ${similarPhoto.user.username}`}
-              />
-             
-            ))} */}
+            <PhotoList photoDatas={photoData?.similar_photos} selectedFavourites={[]} />
+
           </div>
-          
 
 
-          {/* Add any additional details or information here */}
+
         </div>
       )}
     </div>
